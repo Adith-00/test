9f83c35dd5d9c86e58a763272d41a34a971d51d6
Adith-00 <kaadith092@gmail.com>
2024-01-05 14:15:43 +0530

    final commit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import { hero } from "../Assets/Images/image";
 import Button from "./button";
 
-const Hero=()=>{
-    return(
-        <div className="hero">
-        <h1 className="heading">Epic Games : An American Video Game And Software Developer And Publisher Based In Cary, North Carolina.</h1>
-        <h2 className="image">
-            <img src={hero} alt="Hero" />
-        </h2>
-        <div className="heroInfo">
-            <p>Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends. Find it all in Fortnite ... Drop In.</p>
-            <Button BtnText={"Visit Website"} StyleName={"heroButton"}/>
-        </div>
-        
-     </div>
-    )
-}
-export default Hero 
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="heading">
+        Epic Games : An American Video Game And Software Developer And Publisher
+        Based In Cary, North Carolina.
+      </h1>
+      <h2 className="image">
+        <img src={hero} alt="Hero" />
+      </h2>
+      <div className="heroInfo">
+        <p>
+          Create, play, and battle with friends for free in Fortnite. Be the
+          last player standing in Battle Royale and Zero Build, experience a
+          concert or live event, or discover over a million creator made games,
+          including racing, parkour, zombie survival, and more. Each Fortnite
+          island has an individual age rating so you can find the one that's
+          right for you and your friends. Find it all in Fortnite ... Drop In.
+        </p>
+        <Button BtnText={"Visit Website"} StyleName={"heroButton"} />
+      </div>
+    </div>
+  );
+};
+export default Hero;
